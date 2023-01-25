93b41ecbe0bb7450c57da530611dfe9aa8b77437
Slava <118695555+Veaceslava@users.noreply.github.com>
2023-01-26 00:44:36 +0200

    Add files via upload

---
+++ module 14  №4/script.js
@@ -0,0 +1,24 @@
+/* Модудь 14*/
+
+
+/* Модудь 14 / Задание 4 */
+
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+  const value1 = +document.getElementById('num1').value;
+  const value2 = +document.getElementById('num2').value;
+
+  let s = document.getElementById('j-result');
+  s.textContent = '';
+  if (!(value1 >= 100 && value1 <= 300 && value2 >= 100 && value2 <= 300)) {
+    s.textContent = 'одно из чисел вне диапазона от 100 до 300';
+    return;
+  }
+  
+  fetch(`https://picsum.photos/${value1}/${value2}`)
+    .then((response) => {
+      document.getElementById('result').src = response.url;
+    });
+
+});
