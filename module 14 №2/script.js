62da9ddb8ea686f36f2cb781c4709b62db827c6d
Slava <118695555+Veaceslava@users.noreply.github.com>
2023-01-26 00:44:06 +0200

    Add files via upload

---
+++ module 14 №2/script.js
@@ -0,0 +1,10 @@
+/* Модудь 14*/
+
+
+/* Модудь 41 / Задание 2 */
+
+
+const list = '{"name":"Ivan Ivanov","age":35,"prof":"teacher","lang":"en"}'
+undefined
+const b = JSON.parse(list);
+console.log(b);
